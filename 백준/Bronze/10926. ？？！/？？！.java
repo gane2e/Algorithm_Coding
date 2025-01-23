a8d978d78f502ec996f8e71003f84f56a401e57e
가은 <b1a409896@naver.com>
2025-01-23 09:51:37 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	String id = sc.nextLine();
+    	
+    	if(id.equals(id)) {
+    		System.out.println(id + "??!");
+    	}
+    }
+
+}
